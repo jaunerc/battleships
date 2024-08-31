191671c41cc8f9760930dedc74efb1e7ef5b6d63
jaunerc <c.jauner@gmail.com>
2024-08-31 15:19:34 +0200

    add example with express-ws in the server and websockets in the client

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,8 @@
-import express, {Express} from 'express';
+import express from 'express';
+import expressWs from 'express-ws';
 
-const app: Express = express();
+const expressWsInstance = expressWs(express());
+const app = expressWsInstance.app;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -8,8 +10,22 @@ app.use(express.urlencoded({ extended: true }));
 const PORT: number = 3001;
 
 app.get('/api', (_req, res) => {
-    res.status(200).json({ message: 'Hello from the server 2!' });
+    res.status(200).json({ message: 'Hello from the server!' });
 });
+
+app.ws('/', function(ws, _req) {
+    ws.on('message', function(msg) {
+        console.log('server msg received: ' + msg);
+    });
+
+    ws.on('close', function (_code, _reason) {
+        console.log('connection closed')
+    })
+});
+
+expressWsInstance.getWss().on('connection', function () {
+    console.log('new connection')
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
