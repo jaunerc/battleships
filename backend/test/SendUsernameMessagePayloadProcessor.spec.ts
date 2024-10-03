912a63a76fe3e5459b119204212ed707d963f548
jaunerc <c.jauner@gmail.com>
2024-10-03 08:45:04 +0200

    add a test for the SendUsernameMessagePayloadProcessor

---
+++ backend/test/SendUsernameMessagePayloadProcessor.spec.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata'; // this import is necessary so that inversify is working with tests
+import {SendUsernameMessagePayloadProcessor} from "../src/websocket/SendUsernameMessagePayloadProcessor";
+import {GameState} from "../src/GameState";
+import {SendUsernamePayload} from "../../messages/SendUsernamePayload";
+
+describe('SendUsernameMessagePayloadProcessor', () => {
+    describe('process', () => {
+        it('should add a the given player name to the game state', () => {
+            const gameState: GameState = { players: [] }
+            const processor: SendUsernameMessagePayloadProcessor = new SendUsernameMessagePayloadProcessor(gameState)
+
+            const payload: SendUsernamePayload = { name: 'Han Solo' }
+            processor.process(JSON.stringify(payload))
+
+            expect(gameState.players.length).toBe(1)
+            expect(gameState.players[0].name).toBe('Han Solo')
+        })
+    })
+})
