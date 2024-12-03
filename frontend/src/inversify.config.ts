e71bf8e5f7db349365b2d87a9d3fc4d2f72043a4
jaunerc <c.jauner@gmail.com>
2024-12-03 07:23:54 +0100

    set the websocket address based on the current hostname

--- frontend/src/inversify.config.ts
+++ frontend/src/inversify.config.ts
@@ -6,10 +6,12 @@ import {StartView} from "./view/StartView.ts";
 import {PlaceShipsView} from "./view/PlaceShipsView.ts";
 import {GameView} from "./view/GameView.ts";
 
+const websocketAddress: string = `ws://${window.location.hostname}${import.meta.env.VITE_BACKEND_PATH_SUFFIX}`
+
 const container = new Container()
 container.bind<State>('State').toConstantValue(new StateImpl())
-container.bind<WebSocket>('Websocket').toConstantValue(new WebSocket(import.meta.env.VITE_BACKEND_URL))
-
+container.bind<WebSocket>('Websocket').toConstantValue(new WebSocket(websocketAddress))
+console.log(window.location.hostname)
 container.bind<View>('StartView').to(StartView)
 container.bind<View>('UsernameView').to(UsernameView)
 container.bind<View>('PlaceShipsView').to(PlaceShipsView)
