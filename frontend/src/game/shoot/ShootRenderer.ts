60191c2573cacd88895b5246a9e2c3d4f4274814
Cyrill Jauner <c.jauner@gmail.com>
2025-03-01 14:27:19 +0100

    render a different symbol when a player misses a ship (#23)
    * render a different symbol when a player misses a ship
    
    * fix tests

--- frontend/src/game/shoot/ShootRenderer.ts
+++ frontend/src/game/shoot/ShootRenderer.ts
@@ -1,15 +1,21 @@
 import { G } from '@svgdotjs/svg.js'
 import { BoardDimension } from '../GameTypes.ts'
 import { injectable } from 'inversify'
-import { FieldPosition } from '../../../../shared/Shared.ts'
+import { FieldPosition, FireLogEntry } from '../../../../shared/Shared.ts'
 
 @injectable()
 export class ShootRenderer {
     createShootElement(svgShootGroup: G,
         board: BoardDimension,
-        shootPositions: FieldPosition[]): void {
+        fireLogEntries: FireLogEntry[]): void {
         svgShootGroup.clear()
-        shootPositions.forEach(shootPosition => this.renderCross(svgShootGroup, shootPosition, board))
+        fireLogEntries.forEach((fireLogEntry) => {
+            if (fireLogEntry.result === 'hit') {
+                this.renderCross(svgShootGroup, fireLogEntry.coordinates, board)
+            } else {
+                this.renderCircle(svgShootGroup, fireLogEntry.coordinates, board)
+            }
+        })
     }
 
     private renderCross(group: G,
@@ -19,12 +25,22 @@ export class ShootRenderer {
             fieldPosition.y * board.columnSizeInPixels,
             (fieldPosition.x + 1) * board.columnSizeInPixels,
             (fieldPosition.y + 1) * board.columnSizeInPixels)
-            .stroke({ width: 1, color: 'red' })
+            .stroke({ width: 1, color: '#E63946' })
 
         group.line(fieldPosition.x * board.columnSizeInPixels,
             (fieldPosition.y + 1) * board.columnSizeInPixels,
             (fieldPosition.x + 1) * board.columnSizeInPixels,
             fieldPosition.y * board.columnSizeInPixels)
-            .stroke({ width: 1, color: 'red' })
+            .stroke({ width: 1, color: '#E63946' })
+    }
+
+    private renderCircle(group: G,
+        fieldPosition: FieldPosition,
+        board: BoardDimension): void {
+        const halfColumnSize: number = board.columnSizeInPixels / 2
+        group.circle(halfColumnSize / 2)
+            .move(fieldPosition.x * board.columnSizeInPixels + halfColumnSize * 0.75,
+                fieldPosition.y * board.columnSizeInPixels + halfColumnSize * 0.75)
+            .fill('#457B9D')
     }
 }
